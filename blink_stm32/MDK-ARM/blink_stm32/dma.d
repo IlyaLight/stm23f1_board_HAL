eb57078f9150fccd62aa0a644fb91dd2cf79d0b4
IlyaLight <88light88@gmail.com>
2018-01-13 22:51:43 +0300

    up

---
+++ blink_stm32/MDK-ARM/blink_stm32/dma.d
@@ -0,0 +1,29 @@
+blink_stm32\dma.o: ../Src/dma.c
+blink_stm32\dma.o: ../Inc/dma.h
+blink_stm32\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+blink_stm32\dma.o: ../Inc/stm32f1xx_hal_conf.h
+blink_stm32\dma.o: ../Inc/main.h
+blink_stm32\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h
+blink_stm32\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h
+blink_stm32\dma.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
+blink_stm32\dma.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h
+blink_stm32\dma.o: ../Drivers/CMSIS/Include/core_cm3.h
+blink_stm32\dma.o: C:\Program Files\Keil\ARM\ARMCC\Bin\..\include\stdint.h
+blink_stm32\dma.o: ../Drivers/CMSIS/Include/core_cmInstr.h
+blink_stm32\dma.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+blink_stm32\dma.o: ../Drivers/CMSIS/Include/core_cmFunc.h
+blink_stm32\dma.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+blink_stm32\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+blink_stm32\dma.o: C:\Program Files\Keil\ARM\ARMCC\Bin\..\include\stdio.h
+blink_stm32\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h
+blink_stm32\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h
+blink_stm32\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h
+blink_stm32\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h
+blink_stm32\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h
+blink_stm32\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h
+blink_stm32\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h
+blink_stm32\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h
+blink_stm32\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h
+blink_stm32\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+blink_stm32\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_spi.h
+blink_stm32\dma.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h
---
+++ blink_stm32/MDK-ARM/blink_stm32/i2c.d
@@ -0,0 +1,31 @@
+blink_stm32\i2c.o: ../Src/i2c.c
+blink_stm32\i2c.o: ../Inc/i2c.h
+blink_stm32\i2c.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+blink_stm32\i2c.o: ../Inc/stm32f1xx_hal_conf.h
+blink_stm32\i2c.o: ../Inc/main.h
+blink_stm32\i2c.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h
+blink_stm32\i2c.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h
+blink_stm32\i2c.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
+blink_stm32\i2c.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h
+blink_stm32\i2c.o: ../Drivers/CMSIS/Include/core_cm3.h
+blink_stm32\i2c.o: C:\Program Files\Keil\ARM\ARMCC\Bin\..\include\stdint.h
+blink_stm32\i2c.o: ../Drivers/CMSIS/Include/core_cmInstr.h
+blink_stm32\i2c.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+blink_stm32\i2c.o: ../Drivers/CMSIS/Include/core_cmFunc.h
+blink_stm32\i2c.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+blink_stm32\i2c.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+blink_stm32\i2c.o: C:\Program Files\Keil\ARM\ARMCC\Bin\..\include\stdio.h
+blink_stm32\i2c.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h
+blink_stm32\i2c.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h
+blink_stm32\i2c.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h
+blink_stm32\i2c.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h
+blink_stm32\i2c.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h
+blink_stm32\i2c.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h
+blink_stm32\i2c.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h
+blink_stm32\i2c.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h
+blink_stm32\i2c.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h
+blink_stm32\i2c.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+blink_stm32\i2c.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_spi.h
+blink_stm32\i2c.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h
+blink_stm32\i2c.o: ../Inc/gpio.h
+blink_stm32\i2c.o: ../Inc/dma.h
